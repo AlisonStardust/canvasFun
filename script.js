9b242c69a6643c22b5ae5f3cdda577de150767dc
alisonstardust <zakulisami@o2.pl>
2018-04-30 20:40:11 +0200

    sounds, linewidth updated

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const canvas = document.querySelector('.canvas');
 const contextCanvas = canvas.getContext('2d');
-let canvasColors = ['#8367C7', '#427AA1', '#B3001B']
+let canvasColors = ['#8367C7', '#427AA1', '#70A0AF', '#A0C1B9']
+contextCanvas.lineWidth = 150;
+
+var context = new (window.AudioContext || window.webkitAudioContext)();
+
+var oscillator = context.createOscillator();
+
+oscillator.type = 'sine';
+oscillator.frequency.value = 190;
+oscillator.connect(context.destination);
+  oscillator.start();
+  oscillator.stop(1);
+  oscillator.frequency.value += 100;
+// var now = context.currentTime;
+// oscillator.play(now + 1);
+// oscillator.stop(now + 3);
 
 const mouse = {
   x: innerWidth,
@@ -9,6 +24,7 @@ const mouse = {
 
 canvas.width = innerWidth;
 canvas.height = innerHeight;
+
 
 //event listeners on all canvas
 window.addEventListener('resize', function() {
@@ -19,6 +35,7 @@ window.addEventListener('resize', function() {
 document.addEventListener('DOMContentLoaded', function(e) {
   mouse.x = e.x;
   mouse.y = e.y;
+  setInterval(function(){ canvasColors.shift(); }, 3000);
   console.log('document is ready. I can sleep now');
 });
 
@@ -28,20 +45,20 @@ window.addEventListener('mousemove', function(e) {
 });
 
 window.addEventListener('click', function() {
-  canvasColors.shift();
+  // oscillator.start();
+  // oscillator.stop(1);
+  // oscillator.frequency.value += 100;
+  console.log(contextCanvas.lineWidth)
+  //canvasColors.shift();
   changesize();
-  if (canvasColors.length === 0) {
-    canvasColors = ['#8367C7', '#427AA1', "#B3001B"];
-  }
 });
 
 function changesize() {
   requestAnimationFrame(changesize);
-  //contextCanvas.clearRect(0, 0, innerWidth, innerHeight);
-  //contextCanvas.beginPath();
-  //contextCanvas.arc(mouse.x, mouse.y, 20, 0, Math.PI * 2, false);
-  contextCanvas.lineWidth = 15;
-  //contextCanvas.strokeStyle = canvasColors[0];    
+  contextCanvas.lineWidth += 1;
+  if (contextCanvas.lineWidth >= 700) {
+    contextCanvas.lineWidth = 15;
+  }
   contextCanvas.stroke();
 }
 
@@ -50,7 +67,6 @@ function animate() {
   contextCanvas.clearRect(0, 0, innerWidth, innerHeight);
   contextCanvas.beginPath();
   contextCanvas.arc(mouse.x, mouse.y, 20, 0, Math.PI * 2, false);
-  contextCanvas.lineWidth = 150;
   contextCanvas.strokeStyle = canvasColors[0];    
   contextCanvas.stroke();
 }
