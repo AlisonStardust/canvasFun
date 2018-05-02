24eabf9e4ea7b90c6455dad80a519693ffaa48b5
alisonstardust <zakulisami@o2.pl>
2018-05-02 23:07:34 +0200

    gradient update, working on multiple circles

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//variables and canvas settings
 const canvas = document.querySelector('.canvas');
 const contextCanvas = canvas.getContext('2d');
 let canvasColors = ['#8367C7', '#427AA1', '#70A0AF', '#A0C1B9'];
@@ -7,6 +8,9 @@ const mouse = {
   x: innerWidth,
   y: innerHeight
 };
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 //class sound taken from css tricks: https://css-tricks.com/introduction-web-audio-api/
 class Sound {
@@ -37,9 +41,6 @@ class Sound {
   }
 }
 
-canvas.width = innerWidth;
-canvas.height = innerHeight;
-
 //event listeners on all canvas
 window.addEventListener('resize', function() {
   canvas.width = innerWidth;
@@ -49,7 +50,9 @@ window.addEventListener('resize', function() {
 document.addEventListener('DOMContentLoaded', function(e) {
   mouse.x = e.x;
   mouse.y = e.y;
-  setInterval(function(){ canvasColors.shift(); }, 3000);
+  setInterval(function(){
+    canvasColors.shift();
+  }, 3000);
   console.log('document is ready. I can sleep now');
 });
 
@@ -63,17 +66,54 @@ window.addEventListener('mousemove', function(e) {
 });
 
 window.addEventListener('click', function() {
-  let rand = voice[Math.floor(Math.random() * voice.length)];
-  //draw new circle function
-  changesize();
-  contextCanvas.lineWidth = 1;
-  console.log(contextCanvas.lineWidth)
   //sound effects on click
+  let rand = voice[Math.floor(Math.random() * voice.length)];
   let context = new (window.AudioContext || window.webkitAudioContext)();
   let note = new Sound(context);
   let now = context.currentTime;
   note.play(rand, now);
+  //circle functions
+  changesize();
+  contextCanvas.lineWidth = 1;
+  console.log(contextCanvas.lineWidth);
 });
+
+window.addEventListener('click', function() {
+  console.log('as')
+  //animate2();
+  circles.push(new Circle(mouse.x, mouse.y, 0, 0, 10, 20));
+});
+
+//circly objects to make multiple circles
+function Circle(x, y, vx, vy, r, growth) {
+  this.x = x;
+  this.y = y;
+  this.vx = vx;
+  this.vy = vy;
+  this.r = r;
+  this.color = canvasColors[0];
+  this.alpha = 1;
+
+  this.draw = function() {
+    contextCanvas.beginPath();
+    contextCanvas.strokeStyle = this.color.replace('x', + this.alpha);
+    contextCanvas.arc(this.x, this.y, this.r, Math.PI * 2, false);
+    contextCanvas.lineWidth = 2;
+    contextCanvas.stroke();
+    contextCanvas.fillStyle = 'transparent';
+    contextCanvas.fill();
+  }
+
+  this.update = function() {
+    this.x += this.vx;
+    this.y += this.vy;
+    this.alpha -= 0.015;
+    this.r += growth;
+    this.draw();
+  }
+}
+
+var circles = [];
 
 //circles drawing functions
 function changesize() {
@@ -87,12 +127,38 @@ function changesize() {
 function animate() {
   requestAnimationFrame(animate);
   contextCanvas.clearRect(0, 0, innerWidth, innerHeight);
+  var lingrad = contextCanvas.createLinearGradient(0, 0, 0, canvas.height);
+  lingrad.addColorStop(0.1, '#8367C7');
+  lingrad.addColorStop(0.5, '#427AA1');
+  lingrad.addColorStop(0.75, '#70A0AF');
+  lingrad.addColorStop(1, '#A0C1B9');
+  contextCanvas.fillStyle = lingrad;
+  contextCanvas.strokeStyle = lingrad;
   contextCanvas.beginPath();
   contextCanvas.lineWidth += 1;
   contextCanvas.arc(mouse.x, mouse.y, 20, 0, Math.PI * 2, false);
-  contextCanvas.strokeStyle = canvasColors[0];    
+  //contextCanvas.strokeStyle = canvasColors[0];    
   contextCanvas.stroke();
 }
 
+function animate2() {
+  requestAnimationFrame(animate2);
+  contextCanvas.clearRect(0, 0, innerWidth, innerHeight);
+
+  var vx = (Math.random() - 0.5) * 5 + (Math.random() < 0.5 ? -2 : 2);
+  var vy = (Math.random() - 0.5) * 5 + (Math.random() < 0.5 ? -2 : 2);
+  var r = Math.random() * 20 + 30;
+  circles.push(new Circle(mouse.x, mouse.y, vx, vy, r, -0.5));
+  
+  for (let i = 0; i < circles.length; ++i) {
+    circles[i].update();
+    // remove the circle if it is transparent or too small
+    if (circles[i].alpha < 0 || circles[i].r < 3) {
+      circles.splice(i, 1);
+    }
+  }
+}
+
+//main functions starter
 changesize();
 animate();
